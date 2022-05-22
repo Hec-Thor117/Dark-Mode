1fede4e5bb639446a7c03866bc74c3695c586fab
Hector Garnica <106023753+Hec-Thor117@users.noreply.github.com>
2022-05-22 16:58:03 -0500

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const darkModeIcon = document.getElementsByClassName("dark-mode-icon");
 const darkBodyClass = document.body.classList;
 
+// Adding a comment in another branch from Main to practice GitHub pull requests and commits.
+
 darkModeIcon[0].addEventListener("click", () => {
     darkBodyClass.contains("dark-mode") ? 
     darkBodyClass.remove("dark-mode") : 
